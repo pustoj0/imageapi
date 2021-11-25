10becda33b66da44aa473de8cc40ccdb025b96ec
pustoj0 <pustoj0@gmail.com>
2021-11-25 18:31:20 +0200

    first commit

---
+++ src/test/java/voxloud/imageapi/ImageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package voxloud.imageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
